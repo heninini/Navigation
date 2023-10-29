2cfb90ed0cbeedf85b04ae638fd804909a48288b
henos <henoslassie@gmail.com>
2023-10-29 00:48:11 -0700

    project 13

---
+++ src/components/Link.js
@@ -0,0 +1,27 @@
+import classNames from "classnames";
+import { useContext } from "react";
+import NavigationContext from "../context/navigation";
+
+function Link({ to, children, className, activeClassName }) {
+  const { navigate, currentPath } = useContext(NavigationContext);
+
+  const classes = classNames(
+    "text-blue-500",
+    className,
+    currentPath === to && activeClassName
+  );
+  const handleClick = (event) => {
+    if (event.ctrlKey || event.metaKey) {
+      return;
+    }
+    event.preventDefault();
+    navigate(to);
+  };
+
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
+}
+export default Link;
---
+++ src/hooks/use-navigation.js
@@ -0,0 +1,10 @@
+// hooks/use-navigation.js
+import { useContext } from "react";
+import NavigationContext from "../context/navigation";
+
+function useNavigation() {
+  const { navigate } = useContext(NavigationContext);
+  return { navigate };
+}
+
+export default useNavigation;
