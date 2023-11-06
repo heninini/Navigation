c373376ef4272ce5e3d9175abb9aef8a85cd3596
heninini <henos.ghirmay.samuel@gmail.com>
2023-11-05 22:49:51 -0800

    added a useSort hook

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,29 +1,12 @@
-import { useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-
+import useSort from "../hooks/use-sort";
 import Table from "./Table";
 function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
   const { config, data } = props;
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-      setSortOrder("asc");
-      setSortBy(label);
-      return;
-    }
-
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+  const { sortBy, sortOrder, sortedData, setSortColumn } = useSort({
+    data,
+    config,
+  });
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
       return column;
@@ -31,34 +14,14 @@ function SortableTable(props) {
     return {
       ...column,
       header: () => (
-        <th onClick={() => handleClick(column.label)}>
+        <th onClick={() => setSortColumn(column.label)}>
           {getIcons(column.label, sortBy, sortOrder)}
           {column.label}
         </th>
       ),
     };
   });
-
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
-  return (
-    <div>
-      {sortOrder}-{sortBy}
-      <Table {...props} data={sortedData} config={updatedConfig} />
-    </div>
-  );
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
 }
 
 function getIcons(label, sortBy, sortOrder) {
